b44ee306359d32ed0aee6bf86a80d8704b7ef592
Alexander Hoffmann <alexander.hoffmann@artificial-solutions.com>
2020-03-25 11:15:18 +0800

    added some envs

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const qs = require('querystring');
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-const client = require('twilio')(accountSid, authToken);
+
 const TIE = require('@artificialsolutions/tie-api-client');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -34,7 +34,9 @@ const {
   TENEO_ENGINE_URL,
   LANGUAGE_STT,
   LANGUAGE_TTS,
-  PORT
+  PORT,
+  ACCOUNT_SID,
+  AUTH_TOKEN
 } = process.env;
 const port = PORT || 1337;
 const teneoApi = TIE.init(TENEO_ENGINE_URL);
@@ -42,6 +44,8 @@ let language_STT = LANGUAGE_STT || 'en-US'; // See: https://www.twilio.com/docs/
 let language_TTS = LANGUAGE_TTS || 'Polly.Joanna'; // See: https://www.twilio.com/docs/voice/twiml/say/text-speech#amazon-polly
 console.log("LANGUAGE_STT: " + LANGUAGE_STT)
 console.log("LANGUAGE_TTS: " + LANGUAGE_TTS)
+
+const client = require('twilio')(ACCOUNT_SID, AUTH_TOKEN);
 
 // initialise session handler, to store mapping between twillio CallSid and engine session id
 const sessionHandler = SessionHandler();
